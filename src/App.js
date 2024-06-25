0a3eca8e005490578f3eb6102d8eb0231b7544a3
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-06-25 19:30:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Learn from './components/hello';
+import Learning from './components/learning';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+        <Learn/>
+        <Learning/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// functional Components 
---
+++ src/components/learning.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Learning extends Component{
+    render(){
+        return <h1>class components</h1>
+    }
+
+}
+
+export default Learning
+
+
+//class components 
